c8203668ee391c94d34968ecc122437c7083ffd6
TonyFef <fefilov.toha1999@yandex.ru>
2022-06-01 14:41:56 +0300

    made template

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,16 +1,34 @@
+import { APIkey } from "./defaults";
+import { useState } from "react";
+import { atom, useRecoilState } from "recoil";
+
 export const Input = () => {
+    const [temp, setTemp] = useState(""); // data.main.temp
+    const [desr, setDesr] = useState(""); // data.weather.0
+    const [city, setCity] = useState("Quebec"); // data.weather.0
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+
+    const fetchData = () => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setTemp(Math.trunc(+data.main.temp - 273));
+                setDesr(data.weather[0].description);
+                setCity(data.name);
+
+                console.log(data);
+                console.log(temp);
+                console.log(desr);
+                console.log(city);
+            });
+    };
     return (
         <div className="input-sum">
             <div className="input-sum__text">Show the weather in ...</div>
             <div className="input-group">
-                <input
-                    type="text"
-                    className="input-group__field"
-                    placeholder="Search for the city"
-                    aria-label="Search for the city"
-                    // aria-describedby="button-addon2"
-                />
-                <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+                <input type="text" className="input-group__field" placeholder="Search for the city" aria-label="Search for the city" />
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={fetchData}>
                     Subimt
                 </button>
             </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,15 +1,9 @@
-import { APIkey } from "./defaults";
-// import sun from "./sun.svg";
+import React from "react";
+
+import sun from "../sun.png";
 
 export const Main = () => {
-    let city = "Ottawa";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
-
-    const hello = (e) => {
-        let response = fetch(url)
-            .then((res) => res.json())
-            .then((data) => console.log(data));
-    };
+    
 
     return (
         <>
@@ -17,8 +11,10 @@ export const Main = () => {
                 <div className="border-primary">
                     <div className="card-body">
                         <h5 className="card-title">City</h5>
-                        <h3 className="card-temperature">9</h3>
-                        <img src="#" alt="#" />
+                        <h3 className="card-temperature">
+                            9<span className="card-temperature__measure" >&#8451;</span>
+                        </h3>
+                        <img className="card-img" src={sun} alt="" />
                         <span className="card-title">Clear sky</span>
                     </div>
                 </div>
